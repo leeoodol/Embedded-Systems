729cd98dbe72729a0d871fb0d46e114b2ffcf33b
Junho Kim <73748884+junho2000@users.noreply.github.com>
2023-09-08 11:25:40 +0200

    [upload] someip attribute example

--- DES_HU/someip_ex/project-hello/src/HelloWorldService.cpp
+++ DES_HU/someip_ex/project-hello/src/HelloWorldService.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main() {
+    //register service
     std::shared_ptr<CommonAPI::Runtime> runtime = CommonAPI::Runtime::get();
     std::shared_ptr<HelloWorldStubImpl> myService =
     	std::make_shared<HelloWorldStubImpl>();
@@ -14,6 +15,7 @@ int main() {
     std::cout << "Successfully Registered Service!" << std::endl;
 
     while (true) {
+        //current thread sleep (or pause) for 30 seconds. It's a simple way to keep the application running indefinitely while not consuming much CPU.
         std::cout << "Waiting for calls... (Abort with CTRL+C)" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(30));
     }
